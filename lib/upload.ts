b76fb27c5f0f61b593a9264f729c7b60a7d4f6ad
Aminat Ahmed <aminattobiahmed@gmail.com>
2025-09-01 11:48:22 +0100

    Update upload.ts

--- lib/upload.ts
+++ lib/upload.ts
@@ -2,8 +2,8 @@ import { createUploadConfig } from 'pushduck/server';
 
 const { s3 } = createUploadConfig()
   .provider("cloudflareR2",{
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.R2_ACCESS_KEY!,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
     region: 'auto',
     endpoint: process.env.AWS_ENDPOINT_URL!,
     bucket: process.env.S3_BUCKET_NAME!,
